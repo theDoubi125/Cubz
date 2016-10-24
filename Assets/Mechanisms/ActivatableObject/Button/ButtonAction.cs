6063cfc07130d4b93294fbb48c6e19562b9cfcc7
Romain Houpin <romain.houpin@gmail.com>
2016-10-24 20:40:10 +0200

    Refactoring code. -> Add activatable blocks for all activatable object (include block, button, etc)    -> added SimpleActivatableBlock to represent a simple block activated by a mechanism    -> Modify ButtonAction -> Now, it is subclass of ActivatableBlocks class
    Added animation for Button (test)
    Added ability to enable multiple activated mechanism for a single activatable block

---
+++ Assets/Mechanisms/ActivatableObject/Button/ButtonAction.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonAction : ActivatableObject {
+
+	public List<ActivatableObject> targets;
+	public bool stayPressed;
+
+	private bool anim = false;
+	private float initialScaleY;
+
+	// Use this for initialization
+	void Start () {
+		setActivated(false);
+		initialScaleY = transform.localScale.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(anim){
+			float deltaTime = Time.deltaTime;
+
+			if(getActivated()){
+				float diffScale = deltaTime * 0.5F;
+				transform.localScale -= new Vector3 (0, diffScale,0);
+				if (transform.localScale.y <= 0.001) {
+					transform.localScale = new Vector3 (transform.localScale.x, 0.001F, transform.localScale.z);
+					anim = false;
+				}
+			}else{
+				float diffScale = deltaTime * 0.03F;
+				transform.localScale += new Vector3 (0, diffScale,0);
+				if (transform.localScale.y >= initialScaleY) {
+					transform.localScale = new Vector3 (transform.localScale.x, initialScaleY, transform.localScale.z);
+					anim = false;
+				}
+			}
+		}
+	}
+
+	public override void activate(){
+		setActivated(true);
+		anim = true;
+	}
+
+	public override void desactivate(){
+		setActivated(false);
+		if(stayPressed){
+			anim = true;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(!getActivated()){
+			activate();
+			foreach (ActivatableObject ao in targets) {
+				ao.activate();
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(stayPressed){
+			desactivate();
+			foreach (ActivatableObject ao in targets) {
+				ao.desactivate();
+			}
+		}
+	}
+}
---
+++ Assets/Mechanisms/ActivatableObject/SimpleBlock/SimpleActivatableBlock.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleActivatableBlock : ActivatableObject {
+
+	private bool anim = false;
+
+	public int nbActivate;
+	private int currentActivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim) {
+			float deltaTime = Time.deltaTime;
+			float diffScale = deltaTime * 0.8F;
+			if (getActivated()) {
+				
+				transform.localScale -= new Vector3 (diffScale, diffScale, diffScale);
+				transform.Rotate (Vector3.up * 360 * deltaTime);
+				if (transform.localScale.x <= 0) {
+					transform.localScale = new Vector3 (0, 0, 0);
+					transform.gameObject.GetComponent<Collider>().enabled=false;
+					anim = false;
+					}
+			} else {
+				transform.localScale+= new Vector3(diffScale, diffScale, diffScale);
+				transform.Rotate (-Vector3.up*360 * deltaTime);
+				if (transform.localScale.x >= 1) {
+					transform.gameObject.GetComponent<Collider>().enabled=true;
+					transform.localScale = new Vector3 (1F, 1F, 1F);
+					transform.rotation = Quaternion.identity;
+					anim = false;
+				}
+			}
+		}
+	}
+
+	public override void activate(){
+		currentActivate++;
+		if(currentActivate>=nbActivate){
+			setActivated(true);
+			anim = true;
+		}
+
+	}
+
+	public override void desactivate(){
+		currentActivate--;
+		if(currentActivate<nbActivate){
+			setActivated(false);
+			anim = true;
+		}
+	}
+
+}
